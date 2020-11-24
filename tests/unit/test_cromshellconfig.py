4f32059d80f541127041869866ab9f3a7b5725dd
bshifaw <bshifaw@broadinstitute.com>
2020-11-24 07:58:36 -0500

    reset cromshellconfig in every unit test

--- tests/unit/test_cromshellconfig.py
+++ tests/unit/test_cromshellconfig.py
@@ -1,5 +1,7 @@
-from pathlib import Path
+import copy
 from cromshell.utilities import cromshellconfig
+from importlib import reload
+from pathlib import Path
 import pytest
 import os
 
@@ -8,6 +10,7 @@ class TestCromshellConfig:
     """Test the cromshell config values"""
 
     def test_override_slim_metadata_parameters(self):
+        reload(cromshellconfig)
         default_slim_parameters = (
             "=includeKey=id&includeKey=executionStatus&includeKey=backendStatus"
             "&includeKey=status&includeKey=callRoot&expandSubWorkflows=true&includeKey"
@@ -27,27 +30,43 @@ class TestCromshellConfig:
             "following after being specified \n" \
             f"{test_slim_parameters}"
 
-    def test_resolve_cromwell_config_server_address(self, mock_data):
+    def test_resolve_cromwell_config_server_address(self):
+        reload(cromshellconfig)
+        test_cromwell_url = "https://cromwell-v1.dsde-methods.broadinstitute.org"
 
         assert cromshellconfig.cromwell_server is not None, \
             "Cromwell server variable should be set"
 
-        cromshellconfig.resolve_cromwell_config_server_address(server_user="https://cromwell-v1.dsde-methods.broadinstitute.org")
-        assert cromshellconfig.cromwell_server == "https://cromwell-v1.dsde-methods.broadinstitute.org", \
-            "Cromwell server variable should be set to the specified server url "
+        cromshellconfig.resolve_cromwell_config_server_address(
+            server_user=test_cromwell_url)
+        assert cromshellconfig.cromwell_server == test_cromwell_url, \
+            f"Cromwell server variable should be set to the specified server url : {test_cromwell_url}"
 
-        # Save the default submission file path in temp variable
-        temp_submission_file = cromshellconfig.submission_file
+    def test_resolve_cromwell_config_server_address_with_mock_data(self, mock_data_path):
+        reload(cromshellconfig)
+        test_cromwell_url = "https://cromwell-v1.dsde-methods.broadinstitute.org"
+        mock_workflow_id = "d689adec-c600-4e4b-be37-4e30e65848c7"
+        mock_cromwell_url = "https://cromwell-v45.dsde-methods.broadinstitute.org"
+
         # Set the submission file path to mock data
-        cromshellconfig.submission_file = mock_data
+        cromshellconfig.submission_file = os.path.join(mock_data_path,
+                                                       "all.workflow.database.tsv")
         # Execute function that changes cromwell server based off the
         # workflow id and submission file
-        cromshellconfig.resolve_cromwell_config_server_address(workflow_id="d689adec-c600-4e4b-be37-4e30e65848c7")
-        assert cromshellconfig.cromwell_server == "https://cromwell-v45.dsde-methods.broadinstitute.org", \
-             "Cromwell server variable should be set to https://cromwell-v45.dsde-methods.broadinstitute.org"
-        cromshellconfig.submission_file = temp_submission_file
+        cromshellconfig.resolve_cromwell_config_server_address(
+            workflow_id=mock_workflow_id)
+        assert cromshellconfig.cromwell_server == mock_cromwell_url, \
+            f"Cromwell server variable should be set to {mock_cromwell_url}"
+
+        # Cromwell url parameter should supersede workflow id look up
+        cromshellconfig.resolve_cromwell_config_server_address(
+            server_user=test_cromwell_url,
+            workflow_id=mock_workflow_id)
+        assert cromshellconfig.cromwell_server == test_cromwell_url, \
+            "Cromwell server variable should be set to {test_cromwell_url}"
 
     def test_config_dir(self):
+        reload(cromshellconfig)
         assert cromshellconfig.config_dir is not None, "Config variable should be set"
 
         assert cromshellconfig.config_dir == os.path.join(Path.home(), ".cromshell"), \
@@ -57,6 +76,7 @@ class TestCromshellConfig:
             "Config directory should exist "
 
     def test_submission_file(self):
+        reload(cromshellconfig)
         assert cromshellconfig.submission_file is not None, \
             "Submission file variable should be set "
 
@@ -69,6 +89,7 @@ class TestCromshellConfig:
             "Submission file should exist"
 
     def test_cromwell_server(self):
+        reload(cromshellconfig)
         cromshell_config = cromshellconfig
         ret_val = 0
         if cromshell_config.cromwell_server is None:
@@ -77,5 +98,5 @@ class TestCromshellConfig:
         assert ret_val == 0, "Return value should be 0"
 
     @pytest.fixture
-    def mock_data(self):
-        return "tests/unit/mock_data/all.workflow.database.tsv"
+    def mock_data_path(self):
+        return os.path.abspath("mock_data/")
