b5488e976fb027cd183180c7d1a33cba935c48b7
bshifaw <bshifaw@broadinstitute.com>
2021-01-16 11:34:59 -0500

    added status dir for mock data, added tests for status functions

--- tests/unit/test_status.py
+++ tests/unit/test_status.py
@@ -3,30 +3,64 @@ from cromshell.status import command as status_command
 import os
 import json
 
+command_name = "status"
+
 
 class TestStatus:
     """Test the status command functions"""
 
     def test_get_all_call_values_doomed(self, mock_data_path):
+        global command_name
+        function_name = "get_all_call_values"
+
         workflow_metadata_path = os.path.join(mock_data_path,
                                               "doom_workflow_slim_metadata.json")
         with open(workflow_metadata_path, 'r') as f:
             workflow_metadata = json.load(f)
+
         workflow_metadata_status_sum_path = os.path.join(mock_data_path,
-                                                         "status/get_all_call_values/doom_workflow_output.json")
+                                                         command_name,
+                                                         function_name,
+                                                         "doom_workflow_output.json")
         with open(workflow_metadata_status_sum_path, 'r') as f:
             workflow_metadata_status_sum = json.load(f)
 
-        assert status_command.get_metadata_status_summary(
+        assert status_command.get_all_call_values(
             workflow_metadata) == workflow_metadata_status_sum, "Output does not match"
 
-    def test_get_metadata_status_summary_failed(self, mock_data_path):
+    def test_get_all_call_values_running(self, mock_data_path):
+        global command_name
+        function_name = "get_all_call_values"
+
         workflow_metadata_path = os.path.join(mock_data_path,
-                                              "fail_workflow_slim_metadata.json")
+                                              "running_workflow_slim_metadata.json")
+        with open(workflow_metadata_path, 'r') as f:
+            workflow_metadata = json.load(f)
+
+        workflow_metadata_status_sum_path = os.path.join(mock_data_path,
+                                                         command_name,
+                                                         function_name,
+                                                         "running_workflow_output.json")
+        with open(workflow_metadata_status_sum_path, 'r') as f:
+            workflow_metadata_status_sum = json.load(f)
+
+        assert status_command.get_all_call_values(
+            workflow_metadata) == workflow_metadata_status_sum, "Output does not match"
+
+    def test_get_metadata_status_summary_doom(self, mock_data_path):
+        global command_name
+        function_name = "get_metadata_status_summary"
+
+        workflow_metadata_path = os.path.join(mock_data_path,
+                                              command_name,
+                                              function_name,
+                                              "doom_workflow_input.json")
         with open(workflow_metadata_path, 'r') as f:
             workflow_metadata = json.load(f)
         workflow_metadata_status_sum_path = os.path.join(mock_data_path,
-                                                         "failed_workflow_status_summary.json")
+                                                         command_name,
+                                                         function_name,
+                                                         "doom_workflow_output.json")
         with open(workflow_metadata_status_sum_path, 'r') as f:
             workflow_metadata_status_sum = json.load(f)
 
@@ -34,12 +68,19 @@ class TestStatus:
             workflow_metadata) == workflow_metadata_status_sum, "Output does not match"
 
     def test_get_metadata_status_summary_running(self, mock_data_path):
+        global command_name
+        function_name = "get_metadata_status_summary"
+
         workflow_metadata_path = os.path.join(mock_data_path,
-                                              "running_workflow_slim_metadata.json")
+                                              command_name,
+                                              function_name,
+                                              "running_workflow_input.json")
         with open(workflow_metadata_path, 'r') as f:
             workflow_metadata = json.load(f)
         workflow_metadata_status_sum_path = os.path.join(mock_data_path,
-                                                         "running_workflow_status_summary.json")
+                                                         command_name,
+                                                         function_name,
+                                                         "running_workflow_output.json")
         with open(workflow_metadata_status_sum_path, 'r') as f:
             workflow_metadata_status_sum = json.load(f)
 
