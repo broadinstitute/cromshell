d401715532d4082ba5f55dd4c47e72c66f50bdf4
bshifaw <bshifaw@broadinstitute.com>
2021-02-24 08:31:33 -0500

    tox edits

--- tests/unit/test_status.py
+++ tests/unit/test_status.py
@@ -3,31 +3,35 @@ from cromshell.status import command as status_command
 import os
 import json
 
-# Todo: pytest function
+
 class TestStatus:
     """Test the status command functions"""
 
     def test_check_for_failure_doom(self, mock_data_path):
-        workflow_metadata_path = os.path.join(mock_data_path,
-                                              "doom_workflow_slim_metadata.json")
-        with open(workflow_metadata_path, 'r') as f:
+        workflow_metadata_path = os.path.join(
+            mock_data_path, "doom_workflow_slim_metadata.json"
+        )
+        with open(workflow_metadata_path, "r") as f:
             workflow_metadata = json.load(f)
 
-        assert status_command.check_for_failure(
-            workflow_metadata) == "True", "A running workflow metadata should have " \
-                                          "output 'True' to indicate workflow " \
-                                          "has failed."
+        assert status_command.check_for_failure(workflow_metadata) == "True", (
+            "A running workflow metadata should have "
+            "output 'True' to indicate workflow "
+            "has failed."
+        )
 
     def test_check_for_failure_running(self, mock_data_path):
-        workflow_metadata_path = os.path.join(mock_data_path,
-                                              "running_workflow_slim_metadata.json")
-        with open(workflow_metadata_path, 'r') as f:
+        workflow_metadata_path = os.path.join(
+            mock_data_path, "running_workflow_slim_metadata.json"
+        )
+        with open(workflow_metadata_path, "r") as f:
             workflow_metadata = json.load(f)
 
-        assert status_command.check_for_failure(
-            workflow_metadata) == "False", "A running workflow metadata should have " \
-                                           "output 'False' to indicate workflow is " \
-                                           "still running."
+        assert status_command.check_for_failure(workflow_metadata) == "False", (
+            "A running workflow metadata should have "
+            "output 'False' to indicate workflow is "
+            "still running."
+        )
 
     @pytest.fixture
     def mock_data_path(self):
