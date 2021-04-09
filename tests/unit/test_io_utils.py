33e0cbf89b6f71821504a37e2e7094e9f78910f2
bshifaw <bshifaw@broadinstitute.com>
2021-04-09 14:36:34 -0400

    added unit test for create directory and copy files

--- tests/unit/test_io_utils.py
+++ tests/unit/test_io_utils.py
@@ -1,7 +1,10 @@
 import pytest
 
 from cromshell.utilities import io_utils
-from pathlib import Path
+from pathlib import Path, PurePath
+
+import tempfile
+import shutil
 
 
 class TestIOUtilities:
@@ -57,3 +60,90 @@ class TestIOUtilities:
             workflow_id="7ef69ca5-0a9a-4449-8ed1-fce28763712c"), \
             "Should return True with valid ID"
 
+    # Skipping because there isn't logic in function. Testing would
+    # be helpful later if the function ends up supporting file paths to
+    # JSONs or dictionaries as input
+    # def test_pretty_print_json(self):
+
+    def test_create_directory(self, temp_dir_path):
+
+        temp_folder = Path(temp_dir_path + "/test_io_utility/")
+        temp_folder_parents = Path(temp_dir_path + "/test_io_utility/parent1/parent2/")
+
+        # Delete test dir if exists from previous test run
+        if temp_folder.exists():
+            shutil.rmtree(temp_folder)
+
+        # Test that function is able to create a folder.
+        io_utils.create_directory(dir_path=temp_folder, parents=False, exist_ok=False)
+        assert temp_folder.exists(), "Temp folder should have been created"
+
+        # Test that function fails and raises an error because `exist_ok`
+        # is set to `False` and the folder its being asked to create already
+        # exists from the previous lines.
+        with pytest.raises(FileExistsError):
+            io_utils.create_directory(
+                dir_path=temp_folder, parents=False, exist_ok=False
+            ), "Should fail because folder already exists from previous test"
+
+        # Test that exception is not raised in this case even though
+        # folder already exists, because `exist_ok` variable is set to `True`.
+        try:
+            io_utils.create_directory(
+                dir_path=temp_folder, parents=False, exist_ok=True
+            )
+        except Exception as exc:
+            assert False, f"Should not raise exception b/c exist_Ok set to True {exc}"
+
+        # Test that nested folders are not created if `parents` is set to `False`
+        with pytest.raises(FileNotFoundError):
+            io_utils.create_directory(
+                dir_path=temp_folder_parents, parents=False, exist_ok=False
+            ), "Should fail because folder is nested and `parents` is set to `False`"
+
+        # Test that nested folders are created if `parents` is set to `True`
+        io_utils.create_directory(
+            dir_path=temp_folder_parents, parents=True, exist_ok=False
+        )
+        assert temp_folder_parents.exists(), "Temp folder should have been created"
+
+        # Delete temp folder
+        shutil.rmtree(temp_folder)
+
+    def test_copy_files_to_directory(self, temp_dir_path):
+
+        temp_folder = Path(temp_dir_path + "/test_io_utility/")
+        io_utils.create_directory(temp_folder)
+
+        # Test that dummy file should fail
+        with pytest.raises(FileNotFoundError):
+            io_utils.copy_files_to_directory(
+                directory=temp_folder, input_files="/dummy/file"
+            ), "Should fail because folder does not exist"
+
+        # Test that dummy dir should fail
+        file_to_cp = Path(__file__).absolute()
+        with pytest.raises(FileNotFoundError):
+            io_utils.copy_files_to_directory(
+                directory="/dummy/file/", input_files=file_to_cp
+            ), "Should fail because folder does not exist"
+
+        # Test function works with proper dir and file
+        io_utils.copy_files_to_directory(
+            directory=temp_folder, input_files=file_to_cp
+        )
+        copied_file = Path(temp_folder).joinpath(file_to_cp.name)
+        print(copied_file)
+        assert copied_file.exists(), "Temp folder should have been created"
+        # Delete temp folder
+        shutil.rmtree(temp_folder)
+
+    #def test_log_error_and_raise_exception(self):
+
+    @pytest.fixture
+    def mock_data_path(self):
+        return Path(__file__).parent.joinpath("mock_data/")
+
+    @pytest.fixture
+    def temp_dir_path(self):
+        return tempfile.gettempdir()
