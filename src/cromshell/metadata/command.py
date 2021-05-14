22672101d3788e321e6074086ad4592336889d1e
bshifaw <bshifaw@broadinstitute.com>
2021-05-14 08:07:57 -0400

    placed request for metadata in to a function

--- src/cromshell/metadata/command.py
+++ src/cromshell/metadata/command.py
@@ -29,31 +29,49 @@ def main(config, workflow_id, slim_metadata):
 
     config.cromwell_api_workflow_id = f"{config.cromwell_api}/{workflow_id}"
 
-    # Set cromwell server using submission file. Running the function below with
-    # passing only the workflow id overrides the default cromwell url set in the
-    # cromshell config file, command line argument, and environment. This takes
-    # place only if the workflow id is found in the submission file.
+    # Overrides the default cromwell url set in the cromshell config file or
+    # command line argument if the workflow id is found in the submission file.
     cromshellconfig.resolve_cromwell_config_server_address(workflow_id=workflow_id)
 
     # Check if Cromwell Server Backend works
     http_utils.assert_can_communicate_with_server(config)
 
-    # Request workflow status
-    if slim_metadata:
-        request_out = requests.get(
-            f"{config.cromwell_api_workflow_id}/metadata?{slim_metadata}",
+    # Request workflow metadata
+    raw_workflow_metadata = get_workflow_metadata(
+            meta_par=config.METADATA_PARAMETERS,
+            slim_meta=slim_metadata,
+            api_workflow_id=config.cromwell_api_workflow_id,
             timeout=config.requests_connect_timeout,
-            verify=config.requests_verify_certs,
-        )
-    else:
-        request_out = requests.get(
-            f"{config.cromwell_api_workflow_id}/metadata?{config.METADATA_PARAMETERS}",
-            timeout=config.requests_connect_timeout,
-            verify=config.requests_verify_certs,
+            verify_certs=config.requests_verify_certs,
         )
 
-    requested_metadata_json = request_out.content.decode("utf-8")
+    workflow_metadata_json = raw_workflow_metadata.content.decode("utf-8")
 
-    io_utils.pretty_print_json(requested_metadata_json)
+    io_utils.pretty_print_json(workflow_metadata_json)
 
     return 0
+
+
+def get_workflow_metadata(
+    meta_par: str,
+    slim_meta: str,
+    api_workflow_id: str,
+    timeout: int,
+    verify_certs: bool,
+):
+    """Use requests to get the metadata or sub-metadata of
+    a workflow from the cromwell server."""
+
+    # If set, use slim_meta as metadata parameter to be used in api requests
+    if slim_meta:
+        meta_par = slim_meta
+
+    return requests.get(
+        f"{api_workflow_id}/metadata?{meta_par}",
+        timeout=timeout,
+        verify=verify_certs
+    )
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
