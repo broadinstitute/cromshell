c4595a1be8ae1b1837cf85034d72c1db3ad522fd
bshifaw <bshifaw@broadinstitute.com>
2021-06-07 09:17:05 -0400

    Updated test functions to match recent changes to metadata command

--- tests/unit/test_metadata.py
+++ tests/unit/test_metadata.py
@@ -9,18 +9,18 @@ class TestMetadata:
     @pytest.mark.parametrize(
         "test_keys, test_keys_string_out",
         [
-            (["id"], "includeKey=id"),
-            (["id", "status"], "includeKey=id&includeKey=status"),
+            (["id"], {"includeKey": ["id"]}),
+            (["id", "status"], {"includeKey": ["id", "status"]}),
             (
                 ["id", "status", "backendStatus"],
-                "includeKey=id&includeKey=status&includeKey=backendStatus",
+                {"includeKey": ["id", "status", "backendStatus"]},
             ),
         ],
     )
-    def test_process_keys(self, test_keys, test_keys_string_out):
+    def test_combine_keys_and_flags_combine_keys(self, test_keys, test_keys_string_out):
         assert (
-            metadata_command.process_keys_and_flags(
-                test_keys, not_expand_subworkflow=False
+            metadata_command.combine_keys_and_flags(
+                list_of_keys=test_keys, exclude_keys=False, not_expand_subworkflow=False
             )
             == test_keys_string_out
         )
@@ -33,52 +33,80 @@ class TestMetadata:
             ["id", ""],  # Asserts failure on dictionary with empty element
         ],
     )
-    def test_empty_process_keys(self, test_keys):
+    def test_combine_keys_and_flags_empty_keys(self, test_keys):
         with pytest.raises(ValueError):
-            metadata_command.process_keys_and_flags(
-                list_of_keys=test_keys, not_expand_subworkflow=False
+            metadata_command.combine_keys_and_flags(
+                list_of_keys=test_keys, exclude_keys=False, not_expand_subworkflow=False
             ), "Should fail if given empty list."
 
     @pytest.mark.parametrize(
         "test_keys, test_expand_subworkflows, test_keys_string_out",
         [
-            (["id"], False, "includeKey=id"),
-            (["id"], True, "includeKey=id&expandSubWorkflows=true"),
+            (["id"], False, {"includeKey": ["id"]}),
+            (["id"], True, {"includeKey": ["id"], "expandSubWorkflows": "true"}),
         ],
     )
-    def test_process_keys_expand_subworkflows_flag(
+    def test_combine_keys_and_flags_subworkflows_flag(
         self, test_keys, test_expand_subworkflows, test_keys_string_out
     ):
         assert (
-            metadata_command.process_keys_and_flags(
-                test_keys, not_expand_subworkflow=test_expand_subworkflows
+            metadata_command.combine_keys_and_flags(
+                list_of_keys=test_keys,
+                exclude_keys=False,
+                not_expand_subworkflow=test_expand_subworkflows
             )
             == test_keys_string_out
         )
 
     @pytest.mark.parametrize(
-        "test_keys, test_cromshell_config_options, test_config_metadata_param, out_str",
+        "test_keys, test_exclude_keys, test_keys_string_out",
+        [
+            (["id"], False, {"includeKey": ["id"]}),
+            (["id"], True, {"excludeKey": ["id"]}),
+        ],
+    )
+    def test_combine_keys_and_flags_exclude_keys_flag(
+        self, test_keys, test_exclude_keys, test_keys_string_out
+    ):
+        assert (
+            metadata_command.combine_keys_and_flags(
+                list_of_keys=test_keys,
+                exclude_keys=test_exclude_keys,
+                not_expand_subworkflow=False
+            )
+            == test_keys_string_out
+        )
+
+    @pytest.mark.parametrize(
+        "test_keys, "
+        "test_cromshell_config_options, "
+        "test_config_metadata_param, "
+        "out_str, "
+        "use_defaults",
         [
             # Cli keys should be used if provided
             (
                 ["id"],
                 {"metadata_keys": "key"},
-                "Metadata_Keys_Constant",
-                "includeKey=id",
+                {'keys': ["submittedFiles"], 'includeKeys': False},
+                ["id"],
+                False,
             ),
             # Config keys should be used if cli empty
             (
                 [],
                 {"metadata_keys": ["key"]},
-                "Metadata_Keys_Constant",
-                "includeKey=key",
+                {'keys': ["submittedFiles"], 'includeKeys': False},
+                ["key"],
+                False,
             ),
             # Metadata should be used by default if no keys found in cli or config
             (
                 [],
                 {"cromwell": ["stuff"]},
-                "includeKey=Metadata_Keys_Constant",
-                "includeKey=Metadata_Keys_Constant",
+                {'keys': ["submittedFiles"], 'includeKeys': False},
+                ["submittedFiles"],
+                True,
             ),
         ],
     )
@@ -88,13 +116,13 @@ class TestMetadata:
         test_cromshell_config_options,
         test_config_metadata_param,
         out_str,
+        use_defaults,
     ):
         assert (
             metadata_command.resolve_and_return_metadata_keys(
                 cli_key=test_keys,
                 cromshell_config_options=test_cromshell_config_options,
                 config_metadata_param=test_config_metadata_param,
-                not_expand_subworkflow=False,
             )
-            == out_str
+            == out_str, use_defaults
         )
