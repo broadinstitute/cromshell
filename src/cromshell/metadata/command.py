ced546fafc9728a8bdbdc842e55b83e29c1a566f
bshifaw <bshifaw@broadinstitute.com>
2021-06-07 06:28:40 -0400

    Add ability to exclude input keys, moved processes_keys_and_flags function call to module level, removed string concatination in favor for requets params

--- src/cromshell/metadata/command.py
+++ src/cromshell/metadata/command.py
@@ -18,13 +18,29 @@ LOGGER = logging.getLogger(__name__)
     "can be set by add option name before adding key (e.g. '-k id -k status ...')",
 )
 @click.option(
+    "-nes",
     "--not_expand_subworkflow",
     is_flag=True,
     default=True,
     help="Do not expand subworkflow info in metadata",
 )
+@click.option(
+    "-e",
+    "--exclude_keys",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Toggle to either include or exclude keys that are specified "
+         "by the --keys option or in the cromshell config JSON.",
+)
 @click.pass_obj
-def main(config, workflow_id: str, key: list, not_expand_subworkflow: bool):
+def main(
+        config,
+        workflow_id: str,
+        key: list,
+        not_expand_subworkflow: bool,
+        exclude_keys: bool
+):
     """Get the full metadata of a workflow."""
 
     LOGGER.info("metadata")
@@ -39,16 +55,28 @@ def main(config, workflow_id: str, key: list, not_expand_subworkflow: bool):
     http_utils.assert_can_communicate_with_server(config)
 
     # Resolve and get metadata keys from cli, config file, or config default
-    metadata_parameter = resolve_and_return_metadata_keys(
+    metadata_parameter, using_default_meta_param = resolve_and_return_metadata_keys(
         cli_key=key,
         cromshell_config_options=config.cromshell_config_options,
         config_metadata_param=config.METADATA_PARAMETERS,
+    )
+
+    LOGGER.info("Metadata keys set to: %s", metadata_parameter)
+
+    # If default parameter is being used we'll want to set exclude_keys to True
+    # because by default we want to exclude the submittedFiles Field
+    if using_default_meta_param:
+        exclude_keys = True
+
+    processed_metadata_parameter = process_keys_and_flags(
+        list_of_keys=metadata_parameter,
+        exclude_keys=exclude_keys,
         not_expand_subworkflow=not_expand_subworkflow,
     )
 
     # Request workflow metadata
     workflow_metadata_json = get_workflow_metadata(
-        meta_par=metadata_parameter,
+        meta_params=processed_metadata_parameter,
         api_workflow_id=config.cromwell_api_workflow_id,
         timeout=config.requests_connect_timeout,
         verify_certs=config.requests_verify_certs,
@@ -59,78 +87,63 @@ def main(config, workflow_id: str, key: list, not_expand_subworkflow: bool):
     return 0
 
 
-def process_keys_and_flags(list_of_keys: list, not_expand_subworkflow: bool) -> str:
-    """Using a list of cromwell metadata keys as input
-    this function creates a string with appropriate characters
-    that can be used when getting a workflow's metadata"""
-
-    if not list_of_keys:
-        LOGGER.error("Function process_keys_and_flags was given an empty list.")
-        raise ValueError("Function process_keys_and_flags was given an empty list.")
-    else:
-        final_key = ""
-        for key in list_of_keys:
-            if not key:
-                LOGGER.error(
-                    "Function process_keys_and_flags "
-                    "was given an empty element in list."
-                )
-                raise ValueError(
-                    "Function process_keys_and_flags "
-                    "was given an empty element in list."
-                )
-
-            # If string of key is empty add nothing, but if it contains a string
-            # add '&' to prep it for the key that will be added.
-            final_key += "&" if final_key else ""
-
-            # Append key to string of key
-            final_key += f"includeKey={key}"
-
-        if not_expand_subworkflow:
-            final_key += "&expandSubWorkflows=true"
-
-        return final_key
-
-
 def resolve_and_return_metadata_keys(
     cli_key: list,
     cromshell_config_options: dict,
-    config_metadata_param: str,
-    not_expand_subworkflow,
-) -> str:
+    config_metadata_param: dict,
+) -> (list, bool):
     """Determines which metadata keys to use from cli, config file, and default
     parameters, then returns a string of the processed keys ready to be used
-    in an api call."""
+    in an api call. Also returns true if default parameter is being used else false."""
 
     # If keys is specified in cli then use this first
     if cli_key:
         LOGGER.info("Using metadata key(s) from command line options.")
-        LOGGER.info("Metadata keys set to: %s", cli_key)
-        return process_keys_and_flags(
-            list_of_keys=cli_key, not_expand_subworkflow=not_expand_subworkflow
-        )
+        return cli_key, False
 
-    # If timeout is specified in cromshell config file then use it to override default
+    # If metadata_keys is specified in cromshell config file then use it for keys
     elif "metadata_keys" in cromshell_config_options:
         LOGGER.info("Setting metadata key(s) from value in config file.")
-        LOGGER.info(
-            "Metadata keys set to: %s", cromshell_config_options["metadata_keys"]
-        )
-        # Set the requests_connect_timeout variable to timeout value in config file.
-        return process_keys_and_flags(
-            list_of_keys=cromshell_config_options["metadata_keys"],
-            not_expand_subworkflow=not_expand_subworkflow,
-        )
+        return cromshell_config_options["metadata_keys"], False
 
     # Return the default keys from config module constant
     else:
         LOGGER.info("No metadata keys were found in cli or config file.")
-        return config_metadata_param
+        # The default metadata key needs to
+        return config_metadata_param["keys"], True
+
+
+def process_keys_and_flags(
+        list_of_keys: list, exclude_keys: bool, not_expand_subworkflow: bool
+) -> dict:
+    """This functions organises a list of cromwell metadata keys and flags into a
+     dictionary that can passed to requests library"""
+
+    if not list_of_keys:
+        LOGGER.error("Function process_keys_and_flags was given an empty list.")
+        raise ValueError("Function process_keys_and_flags was given an empty list.")
+    elif None in list_of_keys:
+        LOGGER.error(
+            "Function process_keys_and_flags was given a list with empty element."
+        )
+        raise ValueError(
+            "Function process_keys_and_flags was given a list with empty element."
+        )
+    else:
+        # Determines whether the list of keys will be used to exclude or
+        # include fields in the metadata.
+        key_action = "excludeKey" if exclude_keys else "includeKey"
+
+        final_key = {key_action: list_of_keys}
+
+        if not_expand_subworkflow:
+            final_key["expandSubWorkflows"] = "true"
+
+        return final_key
 
 
 def get_workflow_metadata(
-    meta_par: str,
+    meta_params: dict,
     api_workflow_id: str,
     timeout: int,
     verify_certs: bool,
@@ -139,7 +152,10 @@ def get_workflow_metadata(
     a workflow from the cromwell server."""
 
     requests_out = requests.get(
-        f"{api_workflow_id}/metadata?{meta_par}", timeout=timeout, verify=verify_certs
+        f"{api_workflow_id}/metadata",
+        params=meta_params,
+        timeout=timeout,
+        verify=verify_certs,
     )
 
     http_utils.check_http_request_status_code(
