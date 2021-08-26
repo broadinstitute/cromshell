4c9996951712585860ddd394ad9e838be3522825
bshifaw <bshifaw@broadinstitute.com>
2021-08-26 07:25:52 -0400

    fixed lint errors, and corrected yml name for integration

--- tests/integration/test_submit.py
+++ tests/integration/test_submit.py
@@ -1,25 +1,25 @@
 import subprocess
+
 import pytest
 
 
 class TestSubmit:
-
     def test_submit(self, local_cromwell_url: str):
 
         result = subprocess.run(
             [
-                'cromshell',
-                '--cromwell_url',
+                "cromshell",
+                "--cromwell_url",
                 local_cromwell_url,
-                'submit',
-                'tests/workflows/helloWorld.wdl',
-                'tests/workflows/helloWorld.json'
+                "submit",
+                "tests/workflows/helloWorld.wdl",
+                "tests/workflows/helloWorld.json",
             ],
-            capture_output=True
+            capture_output=True,
         )
 
         assert result.returncode == 0
 
     @pytest.fixture
     def local_cromwell_url(self):
-        return 'http://localhost:8000'
+        return "http://localhost:8000"
