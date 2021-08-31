0448b00756cc6f3a0f97c8fc8d4e0d8840d21e53
bshifaw <bshifaw@broadinstitute.com>
2021-08-31 08:02:53 -0400

    parametrized wdl and json, added more log info to be printed

--- tests/integration/test_submit.py
+++ tests/integration/test_submit.py
@@ -5,18 +5,32 @@ from cromshell.__main__ import main_entry as cromshell
 
 
 class TestSubmit:
-    def test_submit(self, local_cromwell_url: str):
-
-        runner = CliRunner()
-        result = runner.invoke(cromshell,
-                               [
-                                   "--cromwell_url",
-                                   local_cromwell_url,
-                                   "submit",
-                                   "tests/workflows/helloWorld.wdl",
-                                   "tests/workflows/helloWorld.json",
-                               ])
-        assert result.exit_code == 0, f"{result.stdout} \n {result.exception}"
+    @pytest.mark.parametrize(
+        "wdl", "json", "exit_code",
+        [
+            ("tests/workflows/helloWorld.wdl", "tests/workflows/helloWorld.json", 0),
+            ("tests/workflows/helloWorld.wdl", "tests/workflows/not_valid.json", 1),
+            ("tests/workflows/not_valid.wdl", "tests/workflows/helloWorld.json", 1),
+        ]
+    )
+    def test_submit(self, local_cromwell_url: str, wdl: str, json: str, exit_code: int):
+        runner = CliRunner(mix_stderr=False)
+        result = runner.invoke(
+            cromshell,
+            [
+                "--cromwell_url",
+                local_cromwell_url,
+                "submit",
+                wdl,
+                json,
+            ],
+        )
+        assert result.exit_code == exit_code, (
+            f"\nSTDOUT:\n{result.stdout}"
+            f"\nSTDERR:\n{result.stderr}"
+            f"\nTraceback:\n{result.exc_info}"
+            f"\nExceptions:\n{result.exception}"
+        )
 
     @pytest.fixture
     def local_cromwell_url(self):
