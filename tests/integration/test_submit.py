b246cb9d3754c3b616ac259cf115d84d131baf24
bshifaw <bshifaw@broadinstitute.com>
2021-10-02 11:14:17 -0400

    combined submit run and workflow id check tests

--- tests/integration/test_submit.py
+++ tests/integration/test_submit.py
@@ -9,8 +9,58 @@ from click.testing import CliRunner
 from cromshell.__main__ import main_entry as cromshell
 
 
+def run_cromshell_submit(
+        wdl: str, json_file: str, local_cromwell_url: str, exit_code: int
+):
+    """Run cromshell submit using CliRunner and assert job is successful"""
+
+    runner = CliRunner(mix_stderr=False)
+    # The absolute path will be passed to the invoke command because
+    # the test is being run in temp directory created by CliRunner.
+    absolute_wdl = str(Path(wdl).resolve())
+    absolute_json = str(Path(json_file).resolve())
+    with runner.isolated_filesystem():
+        result = runner.invoke(
+            cromshell,
+            [
+                "--cromwell_url",
+                local_cromwell_url,
+                "--hide_logo",
+                "submit",
+                absolute_wdl,
+                absolute_json,
+            ],
+        )
+        assert result.exit_code == exit_code, (
+            f"\nSTDOUT:\n{result.stdout}"
+            f"\nSTDERR:\n{result.stderr}"
+            f"\nExceptions:\n{result.exception}"
+            f"\n{print_exception(*result.exc_info)}"
+        )
+        return result
+
+
+def workflow_id_in_txt_db(result, local_workflow_database_tsv: Path):
+    """Get workflow id and assert it is listed in the local workflow database tsv"""
+
+    stdout_substring_formatted = json.loads(result.stdout)
+    test_workflow_id = stdout_substring_formatted["id"]
+
+    # check if workflow id is in database tsv
+    with open(local_workflow_database_tsv, "r") as file1:
+
+        # read file content
+        readfile = file1.read()
+        # checking condition for string found or not
+        if test_workflow_id in readfile:
+            found = True
+        else:
+            found = False
+
+        assert found, "Workflow ID was not found in /all.workflow.database.tsv"
+
+
 class TestSubmit:
-    workflow_id = None  # Anyway to avoid using global variables?
 
     @pytest.mark.parametrize(
         "wdl, json_file, exit_code",
@@ -21,58 +71,22 @@ class TestSubmit:
         ],
     )
     def test_submit(
-        self, local_cromwell_url: str, wdl: str, json_file: str, exit_code: int
+        self, local_cromwell_url: str, wdl: str, json_file: str, exit_code: int,
+        local_hidden_cromshell_folder: Path, local_workflow_database_tsv: Path
     ):
-        runner = CliRunner(mix_stderr=False)
-        # The absolute path will be passed to the invoke command because
-        # the test is being run in temp directory created by CliRunner.
-        absolute_wdl = str(Path(wdl).resolve())
-        absolute_json = str(Path(json_file).resolve())
-        with runner.isolated_filesystem():
-            result = runner.invoke(
-                cromshell,
-                [
-                    "--cromwell_url",
-                    local_cromwell_url,
-                    "--hide_logo",
-                    "submit",
-                    absolute_wdl,
-                    absolute_json,
-                ],
+        # Run cromshell submit
+        result = run_cromshell_submit(
+            wdl=wdl,
+            json_file=json_file,
+            exit_code=exit_code,
+            local_cromwell_url=local_cromwell_url
+        )
+
+        # If submission passed check workflow id in database tsv
+        if exit_code == 0:
+            workflow_id_in_txt_db(
+                result=result, local_workflow_database_tsv=local_workflow_database_tsv
             )
-            assert result.exit_code == exit_code, (
-                f"\nSTDOUT:\n{result.stdout}"
-                f"\nSTDERR:\n{result.stderr}"
-                f"\nExceptions:\n{result.exception}"
-                f"\n{print_exception(*result.exc_info)}"
-            )
-
-            # If submission passed get workflow id and set workflow id global variable
-            if exit_code == 0:
-                stdout_substring_formatted = json.loads(result.stdout)
-                TestSubmit.workflow_id = stdout_substring_formatted["id"]
-
-    def test_workflow_id_in_db(
-        self, local_hidden_cromshell_folder, local_workflow_database_tsv
-    ):
-        test_workflow_id = TestSubmit.workflow_id
-        local_db_file = local_workflow_database_tsv
-
-        # opening a text file
-        file1 = open(local_db_file, "r")
-
-        # read file content
-        readfile = file1.read()
-
-        # checking condition for string found or not
-        if test_workflow_id in readfile:
-            found = True
-        else:
-            found = False
-
-        # closing a file
-        file1.close()
-        assert found, "Workflow ID was not found in /all.workflow.database.tsv"
 
     def test_submit_cromshell_folders_created(
         self, local_server_folder, local_hidden_cromshell_folder
