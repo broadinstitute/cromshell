6f72c6e4867d3dd445c3a99d7fbcf4f582a5b328
bshifaw <bshifaw@broadinstitute.com>
2021-12-01 09:19:17 -0500

    added resolve_workflow_id to alias command, fixed lint

--- src/cromshell/alias/command.py
+++ src/cromshell/alias/command.py
@@ -1,11 +1,10 @@
+import csv
+import fileinput
 import logging
 
 import click
-import csv
-import fileinput
 
 from cromshell.utilities import workflow_id_utils
-
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,26 +13,33 @@ LOGGER = logging.getLogger(__name__)
 @click.argument("workflow_id", required=True)
 @click.argument("alias", required=True)
 @click.pass_obj
-def main(config, workflow_id: str, alias: str):
+def main(config, workflow_id: str or int, alias: str):
     """
     Label the given workflow ID with the given alias.
-    Aliases can be used in place of workflow IDs to reference jobs."
+    Aliases can be used in place of workflow IDs to reference jobs.
+
+    Remove alias by passing empty double quotes as an alias.
+    (e.g. cromshell alias [workflow_id] "")
     """
 
     LOGGER.info("alias")
 
+    resolved_workflow_id = workflow_id_utils.resolve_workflow_id(
+        cromshell_input=workflow_id
+    )
+
     # Perform Checks
     run_alias_pre_checks(
         alias_name=alias,
-        workflow_id=workflow_id,
-        submission_file_path=config.submission_file_path
+        workflow_id=resolved_workflow_id,
+        submission_file_path=config.submission_file_path,
     )
 
     # Set workflow id with given alias
     set_alias_for_workflow_id(
         alias_name=alias,
-        workflow_id=workflow_id,
-        submission_file_path=config.submission_file_path
+        workflow_id=resolved_workflow_id,
+        submission_file_path=config.submission_file_path,
     )
 
     return 0
@@ -46,7 +52,7 @@ def run_alias_pre_checks(alias_name: str, workflow_id: str, submission_file_path
     if not alias_is_valid(alias_name):
         LOGGER.error(
             "Alias %s is invalid, it may not start with a dash or contain whitespace.",
-            alias_name
+            alias_name,
         )
         raise ValueError(
             f"Alias {alias_name} is invalid, it may not start with a dash or contain "
@@ -54,20 +60,18 @@ def run_alias_pre_checks(alias_name: str, workflow_id: str, submission_file_path
         )
 
     # check if alias already exists
-    if alias_exists(
-            alias_name=alias_name, submission_file=submission_file_path
-    ):
+    if alias_exists(alias_name=alias_name, submission_file=submission_file_path):
         LOGGER.error("Can't use %s as alias, as it is already being used.", alias_name)
         raise ValueError(f"Alias already exists: {alias_name} ")
 
     # check if workflow id exists
     if not workflow_id_utils.workflow_id_exists(
-            workflow_id=workflow_id,
-            submission_file=submission_file_path
+        workflow_id=workflow_id, submission_file=submission_file_path
     ):
         LOGGER.error("Could not find workflow id %s in submission file.", workflow_id)
         raise ValueError(
-            f"Could not find workflow id {workflow_id} in submission file.")
+            f"Could not find workflow id {workflow_id} in submission file."
+        )
 
 
 def alias_is_valid(alias_name: str) -> bool:
@@ -80,7 +84,7 @@ def alias_is_valid(alias_name: str) -> bool:
 
 def alias_exists(alias_name: str, submission_file) -> bool:
     """Check if alias name already exists in submission file"""
-    with open(submission_file, 'r') as csv_file:
+    with open(submission_file, "r") as csv_file:
         reader = csv.DictReader(csv_file, delimiter="\t")
         for row in reader:
             if row["ALIAS"] == alias_name:
@@ -89,18 +93,19 @@ def alias_exists(alias_name: str, submission_file) -> bool:
 
 
 def set_alias_for_workflow_id(
-        alias_name: str, workflow_id: str, submission_file_path: str
+    alias_name: str, workflow_id: str, submission_file_path: str
 ):
     """Set the alias name of a workflow id in the submission file"""
+
+    LOGGER.info("Setting workflow %s alias to '%s'", workflow_id, alias_name)
+
     with fileinput.FileInput(
         submission_file_path, inplace=True, backup=".bak"
     ) as csv_file:
         reader = csv.DictReader(csv_file, delimiter="\t")
         print("\t".join(reader.fieldnames))
         for row in reader:
-            if (
-                row["RUN_ID"] == workflow_id
-            ):
+            if row["RUN_ID"] == workflow_id:
                 row["ALIAS"] = alias_name
                 print("\t".join(x for x in row.values() if x))
             else:
--- src/cromshell/utilities/workflow_id_utils.py
+++ src/cromshell/utilities/workflow_id_utils.py
@@ -1,23 +1,23 @@
-from cromshell.utilities import io_utils, cromshellconfig
-import logging
 import csv
+import logging
+
+from cromshell.utilities import cromshellconfig, io_utils
 
 LOGGER = logging.getLogger(__name__)
 
 
-def resolve_workflow_id(cromshell_input: str or int) -> str:
+def resolve_workflow_id(cromshell_input: str) -> str:
     if io_utils.is_workflow_id_valid(cromshell_input):
         return cromshell_input
-    elif cromshell_input is int:
-        return obtain_workflow_id_using_digit(cromshell_input)
+    elif cromshell_input.strip("-").isnumeric():
+        return obtain_workflow_id_using_digit(int(cromshell_input))
     else:
         return obtain_workflow_id_using_alias(cromshell_input)
 
 
 def obtain_workflow_id_using_digit(relative_id: int) -> str:
-    LOGGER.info(
-        "Get workflow id from submission file using relative_id."
-    )
+    """Get workflow id from submission file using relative_id."""
+    LOGGER.info("Get workflow id from submission file using relative_id.")
     with open(cromshellconfig.submission_file_path, "r") as csv_file:
         reader = csv.DictReader(csv_file, delimiter="\t")
         total_lines = len(list(reader))
@@ -27,8 +27,9 @@ def obtain_workflow_id_using_digit(relative_id: int) -> str:
         if relative_id == 0:
             row_index = total_lines
         else:
-            row_index = relative_id if relative_id > 0 else total_lines - (
-                    abs(relative_id) - 1)
+            row_index = (
+                relative_id if relative_id > 0 else total_lines - (abs(relative_id) - 1)
+            )
 
         for i, row in enumerate(reader):
             if i == row_index:
@@ -38,17 +39,17 @@ def obtain_workflow_id_using_digit(relative_id: int) -> str:
         LOGGER.error(
             "Unable to use relative id value '%i' to obtain workflow id. The total rows"
             " in submission file is : %i, relative id is most likely output of bounds,",
-            relative_id, total_lines
+            relative_id,
+            total_lines,
         )
         raise ValueError(
-            f"Unable to use relative id value '{relative_id}' to obtain workflow id")
+            f"Unable to use relative id value '{relative_id}' to obtain workflow id"
+        )
 
 
 def obtain_workflow_id_using_alias(alias_name: str) -> str:
     """Get workflow id from submission file using alias"""
-    LOGGER.info(
-        "Get workflow id from submission file using alias."
-    )
+    LOGGER.info("Get workflow id from submission file using alias.")
     with open(cromshellconfig.submission_file_path, "r") as csv_file:
         reader = csv.DictReader(csv_file, delimiter="\t")
         for row in reader:
@@ -56,16 +57,16 @@ def obtain_workflow_id_using_alias(alias_name: str) -> str:
                 return row["RUN_ID"]
 
         LOGGER.error(
-            "Unable to find alias '%s' in submission file '%s'", alias_name,
-            cromshellconfig.submission_file_path
+            "Unable to find alias '%s' in submission file '%s'",
+            alias_name,
+            cromshellconfig.submission_file_path,
         )
-        raise ValueError(
-            f"Unable to find alias {alias_name} in submission file")
+        raise ValueError(f"Unable to find alias {alias_name} in submission file")
 
 
 def workflow_id_exists(workflow_id: str, submission_file) -> bool:
     """Check if workflow ID exists in submission file."""
-    with open(submission_file, 'r') as csv_file:
+    with open(submission_file, "r") as csv_file:
         reader = csv.DictReader(csv_file, delimiter="\t")
         for row in reader:
             if row["RUN_ID"] == workflow_id:
