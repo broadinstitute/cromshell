a9d84706ebefb365f3d3f7e50ebf7b86b8c16cb2
bshifaw <bshifaw@broadinstitute.com>
2021-12-09 08:56:23 -0500

    added unit test for 'check_workflow_has_alias'

--- tests/unit/test_alias.py
+++ tests/unit/test_alias.py
@@ -17,11 +17,6 @@ def submission_file(mock_data_path):
     return mock_data_path.joinpath("all.workflow.database.tsv")
 
 
-# @pytest.fixture
-# def temp_dir_path():
-#     return Path(tempfile.gettempdir() + "/test_io_utility/")
-
-
 class TestAlias:
     """Test the alias command functions"""
 
@@ -30,7 +25,10 @@ class TestAlias:
         [
             ["goodalias", True],
             ["-badalias", False],
+            ["good-alias", True],
+            ["goodalias-", True],
             ["bad alias", False],
+            ["321", False],
         ],
     )
     def test_alias_is_valid(self, alias_name: str, alias_validity: bool) -> None:
@@ -57,7 +55,26 @@ class TestAlias:
             is alias_existence
         )
 
-    # tmp_path is a default pytest fixture
+    @pytest.mark.parametrize(
+        "workflow_id, alias_existence",
+        [
+            ["a63aa10c-a43e-4ca7-9be9-c2d2aa08b96d", True],
+            ["b3b197b3-fdca-4647-9fd8-bf16d2cb734d", False],
+        ],
+    )
+    #  'caplog' is pytest fixture
+    def test_check_workflow_has_alias(
+        self, workflow_id, alias_existence, submission_file, caplog
+    ) -> None:
+        alias_command.check_workflow_has_alias(
+            workflow_id=workflow_id, submission_file=submission_file
+        )
+        for record in caplog.records:
+            assert record.levelname == "WARNING"
+            if alias_existence:
+                assert "Workflow already has alias, its current alias" in caplog.text
+
+    # 'tmp_path' is a default pytest fixture
     def test_set_alias_for_workflow_id(
         self, mock_data_path, submission_file, tmp_path
     ) -> None:
