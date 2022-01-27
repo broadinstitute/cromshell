d82211f73f0d054928c92fe50c8e4ae71629a55a
bshifaw <bshifaw@broadinstitute.com>
2022-01-27 08:09:01 -0500

    added the use of tsv header enum, assertion for failed command

--- tests/integration/test_alias.py
+++ tests/integration/test_alias.py
@@ -4,8 +4,10 @@ from traceback import print_exception
 
 import pytest
 from click.testing import CliRunner
+import hashlib
 
 from cromshell.__main__ import main_entry as cromshell
+from cromshell.utilities.cromshellconfig import AllWorkflowDatabaseColumns
 
 
 def run_cromshell_alias(workflow_id: str, alias_name: str, exit_code: int):
@@ -19,6 +21,7 @@ def run_cromshell_alias(workflow_id: str, alias_name: str, exit_code: int):
             cromshell,
             [
                 "alias",
+                "--",
                 workflow_id,
                 alias_name,
             ],
@@ -41,23 +44,24 @@ def assert_workflow_id_matches_alias(
     with open(local_workflow_database_tsv, "r") as csv_file:
         reader = csv.DictReader(csv_file, delimiter="\t")
         for row in reader:
-            if row["RUN_ID"] == workflow_id:
+            if row[AllWorkflowDatabaseColumns.Run_ID.value] == workflow_id:
                 #  If alias_name is "" (which is when we want to remove an alias) assert
                 #  that the alias for the workflow id returns None. Else (when adding
                 #  and alias) assert that the alias for the workflow id returns
                 #  the matching alias name.
                 if alias_name == "":
-                    assert row["ALIAS"] is None
+                    assert row[AllWorkflowDatabaseColumns.Alias.value] is None
                 else:
-                    assert row["ALIAS"] == alias_name
+                    assert row[AllWorkflowDatabaseColumns.Alias.value] == alias_name
 
 
 class TestAlias:
     @pytest.mark.parametrize(
         "workflow_id, alias_name, exit_code",
         [
-            ("b3b197b3-fdca-4647-9fd8-bf16d2cb734d", "breath", 0),
-            ("a63aa10c-a43e-4ca7-9be9-c2d2aa08b96d", "", 0),
+            ("b3b197b3-fdca-4647-9fd8-bf16d2cb734d", "breath", 0),  # adding alias
+            ("a63aa10c-a43e-4ca7-9be9-c2d2aa08b96d", "", 0),  # removing alias
+            ("-1", "dart", 0),  # testing relative Ids
             (
                 "682f3e72-0285-40ec-8128-1feb877706ce",
                 "wombat",
@@ -91,3 +95,6 @@ class TestAlias:
                 alias_name=alias_name,
                 local_workflow_database_tsv=local_workflow_database_tsv,
             )
+        else:  # because command failed md5sum of mock and local should be the same
+            assert hashlib.md5(open(local_workflow_database_tsv, 'rb').read()).hexdigest() == \
+                   hashlib.md5(open(mock_workflow_database_tsv, 'rb').read()).hexdigest()
