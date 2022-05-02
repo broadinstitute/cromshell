2077c07d0c7bfcaae0ae713a6764c1eb08f8c967
Louis Bergelson <louisb@broadinstitute.org>
2022-05-02 17:31:51 -0400

    update

--- src/cromshell/utilities/animate.py
+++ src/cromshell/utilities/animate.py
@@ -1,7 +1,8 @@
+import math
+import random
 import time
 import os
 import copy
-
 
 turtleRight = """
                __  
@@ -21,11 +22,7 @@ turtleLeft = """
  ""    ""     ""
  """
 
-grass = """
-                                                                                            
-                                                                                            
-                                                                                            
-                                                                                            
+grass = """                                                                              
  |      |                  \   /     \         |      |                            \        
 ||\||  ||||/// |||\ ||| \||\ \|/ | \ ||| \|| /||\||  ||||/// |||\ ||| \||\ \|/ | \ ||| \|| /
 """
@@ -49,41 +46,25 @@ def init_grid(width, height, value):
 
 
 class AsciiImage:
-
-    def __init__(self, art: str, color: int, transparentChar = None):
+    def __init__(self, chars: list[list[str]], colors: list[list[int]], transparentChar: str = None):
         self.transparentChar = transparentChar
-        lines = art.splitlines()
-        lines = [x for x in lines if len(x) > 0]
-
-        self.height: int = len(lines)
-        self.width: int = len(lines[0])
-        assert color < 256
-        self.colors = init_grid(self.width, self.height, color)
-        self.chars = init_grid(self.width, self.height, " ")
-
-        for y, line in enumerate(lines):
-            for x, char in enumerate(line):
-                self.chars[y][x] = char
-
-    def init(self, width, height):
-        self.transparentChar = " "
-        self.width = width
-        self.height = height
-        self.colors = init_grid(self.width, self.height, 0)
-        self.chars = init_grid(self.width, self.height, " ")
+        self.height: int = len(colors)
+        self.width: int = len(colors[0])
+        self.colors = colors
+        self.chars = chars
 
     def getChar(self, x: int, y: int) -> str:
-        return self.chars[self.height-y-1][x]
+        return self.chars[self.height - y - 1][x]
 
     def setChar(self, x: int, y: int, value: str) -> None:
-        self.chars[self.height-y-1][x] = value
+        self.chars[self.height - y - 1][x] = value
 
     def getColor(self, x: int, y: int) -> int:
-        return self.colors[self.height-y-1][x]
+        return self.colors[self.height - y - 1][x]
 
     def setColor(self, x: int, y: int, value: int) -> None:
         assert value < 256
-        self.colors[self.height-y-1][x] = value
+        self.colors[self.height - y - 1][x] = value
 
     def getPixel(self, x: int, y: int) -> (str, int):
         return (self.getChar(x, y), self.getColor(x, y))
@@ -93,8 +74,31 @@ class AsciiImage:
         self.setChar(x, y, char)
         self.setColor(x, y, color)
 
-    def isTransparent(self, x: int, y:int)-> bool:
-        return self.getChar(x,y) == self.transparentChar
+    def isTransparent(self, x: int, y: int) -> bool:
+        return self.getChar(x, y) == self.transparentChar
+
+
+def from_ascii(art: str, color: int, transparentChar=" "):
+    transparentChar = transparentChar
+    lines = art.splitlines()
+    lines = [x for x in lines if len(x) > 0]
+
+    height: int = len(lines)
+    width: int = len(lines[0])
+    assert color < 256
+    colors = init_grid(width, height, color)
+    chars = init_grid(width, height, " ")
+
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            chars[y][x] = char
+    return AsciiImage(chars, colors, transparentChar)
+
+
+def make_blank(width, height):
+    colors = init_grid(width, height, 0)
+    chars = init_grid(width, height, " ")
+    return AsciiImage(chars, colors, " ")
 
 
 test = """123a
@@ -103,9 +107,9 @@ test = """123a
 
 overlay_test = """ab
 cd"""
-overlay_testImage = AsciiImage(overlay_test, 35)
+overlay_testImage = from_ascii(overlay_test, 35)
 
-testImage = AsciiImage(test, 6)
+testImage = from_ascii(test, 6)
 
 
 def print_image(image: AsciiImage):
@@ -113,11 +117,11 @@ def print_image(image: AsciiImage):
         out = ""
         last_color = None
         for x in range(0, image.width):
-            color = image.getColor(x, image.height-y-1)
+            color = image.getColor(x, image.height - y - 1)
             if last_color is not color:
                 out += get_color_by_code(color)
                 last_color = color
-            out += image.getChar(x, image.height-y-1)
+            out += image.getChar(x, image.height - y - 1)
         out += RESET
         print(out)
 
@@ -125,8 +129,9 @@ def print_image(image: AsciiImage):
 def composite(base: AsciiImage, overlay: AsciiImage, x: int, y: int):
     for j in range(0, overlay.height):
         for i in range(0, overlay.width):
-            if not overlay.isTransparent(i,j):
-                base.setPixel(x + i, y + j, overlay.getPixel(i, j))
+            if x + i < base.width and y + j < base.height:
+                if not overlay.isTransparent(i, j):
+                    base.setPixel(x + i, y + j, overlay.getPixel(i, j))
     return base
 
 
@@ -135,43 +140,77 @@ flower_yellow_base = """
 \|/
 """
 
-flower_yellow_Image = AsciiImage(flower_yellow_base, 40)
+flower_yellow_Image = from_ascii(flower_yellow_base, 40)
 flower_yellow_Image.setColor(1, 1, 11)
 
 flower_purple_base = """
 8
 |
 """
-flower_purple_image = AsciiImage(flower_purple_base, 36)
-flower_purple_image.setColor(0,1,207)
+flower_purple_image = from_ascii(flower_purple_base, 36)
+flower_purple_image.setColor(0, 1, 207)
+
+grass1 = from_ascii(
+    """
+
+|
+""", 40)
+
+grass2 = from_ascii(
+    """
+|
+|
+""", 40)
+
+grass3 = from_ascii(
+    """
+ 
+/
+""", 40)
+
+grass4 = from_ascii(
+    """
+ 
+\
+""", 40)
+
+grass5 = from_ascii(
+    """
+ 
+ 
+""", 40)
+
 
 def generateBackground(width: int) -> AsciiImage:
-    blank = AsciiImage(width, 7)
-    composite(blank, grass,)
+    blank = make_blank(width, 7)
+    grassElements = random.choices(
+        [grass1, grass2, grass3, grass4, grass5],
+        weights=[10, 2, 2, 2, 1],
+        k=width
+    )
+    for i, element in enumerate(grassElements):
+        composite(blank, element, i, 0)
+    return blank
+
+def get_flower_overlay(flower: AsciiImage, percent, width):
+    actual_percent = min(random.gauss(percent, percent / 2), 100)
+    number_of_flowers = math.floor((actual_percent / 100) * width)
+    flowers = make_blank(width, flower.height)
+    for i in range(0, number_of_flowers):
+        composite(flowers, flower, random.randint(0,width), 0)
+    return flowers
+
 def animateTurtle():
-    turtleRightImage = AsciiImage(turtleRight, 28, " ")
-    grassImage = AsciiImage(grass, 46)
+    turtleRightImage = from_ascii(turtleRight, 28, " ")
+    width = os.get_terminal_size().columns
 
-    composite(grassImage, flower_yellow_Image, 10,0)
-    composite(grassImage, flower_yellow_Image, 30, 0)
-    composite(grassImage, flower_purple_image, 14, 0)
-    composite(grassImage, flower_purple_image, 15, 1)
-    composite(grassImage, flower_purple_image, 15, 0)
-    composite(grassImage, flower_purple_image, 16, 0)
-
-    composite(grassImage, flower_purple_image, 54, 1)
-    composite(grassImage, flower_purple_image, 56, 0)
-    composite(grassImage, flower_purple_image, 59, 0)
-
-    composite(grassImage, flower_yellow_Image, 70, 0)
-    composite(grassImage, flower_yellow_Image, 71, 0)
-
-    composite(grassImage, flower_yellow_Image, 87, 0)
-    composite(grassImage, flower_yellow_Image, 60, 0)
+    grassImage = generateBackground(width)
+    composite(grassImage, get_flower_overlay(flower_yellow_Image, 5, width), 0,0)
+    composite(grassImage, get_flower_overlay(flower_purple_image, 10, width), 0, 0)
 
     print(grassImage.height)
     print(turtleRightImage.height)
-    assert turtleRightImage.height == grassImage.height
+
     nlines = grassImage.height
 
     # scroll up to make room for output
@@ -182,7 +221,6 @@ def animateTurtle():
 
     # save current cursor position
     print("\033[s", end="")
-    width = os.get_terminal_size().columns
 
     for t in range(0, width - turtleRightImage.width):
         # restore saved cursor position
