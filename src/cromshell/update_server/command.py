749f121e75a6e9933aa1503fe80b727e7766c412
Stephen Fleming <sjf11@case.edu>
2022-05-03 11:23:22 -0400

    Addressed review comments

--- src/cromshell/update_server/command.py
+++ src/cromshell/update_server/command.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 from pathlib import Path
+from typing import Dict
 
 import click
 
@@ -15,6 +16,8 @@ CROMWELL_SERVER_KEY = "cromwell_server"
 def main(config, cromwell_server_url):
     """Update the default Cromwell server in the config file"""
 
+    LOGGER.info("update-server")
+
     # location of the config file
     cromshell_config_path = os.path.join(
         config.config_dir, config.CROMSHELL_CONFIG_FILE_NAME
@@ -24,18 +27,18 @@ def main(config, cromwell_server_url):
     ), f"Cannot write to Cromshell config file {cromshell_config_path}"
 
     # the contents of the config file as a dict
-    cromshell_config_dict = config.cromshell_config_options
+    cromshell_config: Dict = config.cromshell_config_options
 
     # update the cromwell server
-    if CROMWELL_SERVER_KEY not in cromshell_config_dict.keys():
-        cromshell_config_dict.update({CROMWELL_SERVER_KEY: cromwell_server_url})
+    if CROMWELL_SERVER_KEY not in cromshell_config.keys():
+        cromshell_config.update({CROMWELL_SERVER_KEY: cromwell_server_url})
     else:
-        cromshell_config_dict[CROMWELL_SERVER_KEY] = cromwell_server_url
+        cromshell_config[CROMWELL_SERVER_KEY] = cromwell_server_url
 
-        # write the modified config file
-        config_contents = json.dumps(cromshell_config_dict, indent=2)
-        with Path(cromshell_config_path).open("w") as crom_config_file:
-            crom_config_file.write(config_contents)
+    # write the modified config file
+    config_contents = json.dumps(cromshell_config, indent=2)
+    with Path(cromshell_config_path).open("w") as crom_config_file:
+        crom_config_file.write(config_contents)
 
     LOGGER.info(f"Cromshell config file at {cromshell_config_path}")
     LOGGER.info(f"Default Cromwell server updated to {cromwell_server_url}")
