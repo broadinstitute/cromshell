2de64aab99497a77098c4b27cbc63f7ad12e984c
bshifaw <bshifaw@broadinstitute.com>
2022-10-07 12:45:54 -0400

    scripts to publish to pypi

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ from setuptools import find_packages, setup
 with open("requirements.txt") as fh:
     install_requires = fh.readlines()
 
+with open("test-requirements.txt") as fh:
+    test_install_requires = fh.readlines()
+    test_install_requires.remove("-r requirements.txt")
+    test_install_requires.extend(install_requires)
+
 with open("README.md") as fh:
     long_description = fh.read()
 
@@ -21,14 +26,14 @@ setup(
     license="BSD 3-Clause",
     long_description=long_description,
     install_requires=install_requires,
-    tests_require=["coverage", "pytest"],
+    tests_require=test_install_requires,
     python_requires=">=3.7",
     packages=find_packages("src"),
     package_dir={"": "src"},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD 3-Clause",
+        "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.7",
