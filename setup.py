9c99f4db2966f66a5f146b9f0009b0a4ab191a8a
bshifaw <bshifaw@broadinstitute.com>
2023-10-17 14:08:29 -0400

    Bump version: 2.0.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md") as fh:
 # https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure
 
 # Version number is automatically set via bumpversion. DO NOT MODIFY:
-version = "2.0.0"
+version = "2.1.0"
 setup(
     name="cromshell",
     version=version,
